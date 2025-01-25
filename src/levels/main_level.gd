b6034bf70679be1edb07e96340c74c8f2585cd31
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-01-24 20:22:48 -0800

    Update zoom on window size change

--- src/levels/main_level.gd
+++ src/levels/main_level.gd
@@ -8,11 +8,16 @@ var horizontal_speed := 200
 
 
 func _ready() -> void:
-    var viewport_size: Vector2 = G.main.get_viewport().size
-    var viewport_basis_ratio := viewport_size / VIEWPORT_SIZE_BASIS
-    var zoom: float = max(viewport_basis_ratio.x, viewport_basis_ratio.y)
-    %Camera2D.zoom = Vector2.ONE * zoom
+    _update_zoom()
+    get_tree().get_root().size_changed.connect(_update_zoom)
 
 
 func _physics_process(delta: float) -> void:
     %Player.position.x += horizontal_speed * delta
+
+
+func _update_zoom() -> void:
+    var viewport_size: Vector2 = G.main.get_viewport().size
+    var viewport_basis_ratio := viewport_size / VIEWPORT_SIZE_BASIS
+    var zoom: float = max(viewport_basis_ratio.x, viewport_basis_ratio.y)
+    %Camera2D.zoom = Vector2.ONE * zoom
