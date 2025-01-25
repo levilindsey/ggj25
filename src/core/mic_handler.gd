54b4cfbd81cc6f2aa187a61b463e96af7b5e5e0d
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-01-24 18:02:35 -0800

    Add some mic changes

--- src/core/mic_handler.gd
+++ src/core/mic_handler.gd
@@ -8,16 +8,38 @@ const PROCESS_EFFECT_INDEX := 0
 
 # Magic value copied from the Godot Audio Spectrum Demo.
 # Probably something relating to Fourier something something...
-const FREQUENCY_MAX = 11050.0
-const FREQUENCY_MIN = 0.0
+const FREQUENCY_MAX := 100.0
+const FREQUENCY_MIN := 00.0
+
+# Levi's mic:
+const LOW_MAGNITUDE := 0.001
+const HIGH_MAGNITUDE := 0.08
+
+# Alden's mic:
+#const LOW_MAGNITUDE := 0.0002
+#const HIGH_MAGNITUDE := 0.0
+
+const MIC_SAMPLE_PERIOD := 0.05
+const MIC_PRINT_PERIOD := 0.3
 
 var log_mic_debugging := true
 
 var spectrum: AudioEffectSpectrumAnalyzerInstance
 
-var _throttled_process: Callable
+var _throttled_sample: Callable
+var _throttled_print: Callable
 
-var _latest_max_magnitude := 0.0
+# -   This value is reset every MIC_SAMPLE_PERIOD seconds.
+# -   It represents the max magnitude of the _current_ partial time window.
+var _in_progress_max_magnitude := 0.0
+
+# -   This value is updated every MIC_SAMPLE_PERIOD seconds.
+# -   It represents the max magnitude of the _previous_ time window.
+var latest_magnitude := 0.0
+
+
+# TODO: Consider adding a screen at the start for calibrating the light blow and
+#       the hard blow levels (or from Settings).
 
 
 func _ready() -> void:
@@ -26,7 +48,8 @@ func _ready() -> void:
     var process_bus_index := AudioServer.get_bus_index(PROCESS_BUS_NAME)
     spectrum = AudioServer.get_bus_effect_instance(process_bus_index, PROCESS_EFFECT_INDEX)
 
-    _throttled_process = S.time.throttle(_process_throttled, 0.3, false)
+    _throttled_sample = S.time.throttle(_sample_throttled, MIC_SAMPLE_PERIOD, false)
+    _throttled_print = S.time.throttle(_print_throttled, MIC_PRINT_PERIOD, false)
 
 
 func _process(_delta: float) -> void:
@@ -41,14 +64,28 @@ func _process(_delta: float) -> void:
     #       amplitude thresholds based on the player's particular input
     #       (hardware, gain, environment, and voice can probably all differ
     #       wildly in terms of overall amplitude)?
+    # TODO: Detect if amplitude is exactly 0.0, and show a mic-must-be-hooked-up-and-allowed screen.
+    #       -   Auto-close screen when amplitude becomes >0.
+    #       -   Disallow closing the screen otherwise.
     # TODO: Show some sort of amplitude indicator in the hud (show the smoothed signal).
     # TODO: Do something with this signal.
 
-    _latest_max_magnitude = max(_latest_max_magnitude, magnitude)
-    _throttled_process.call()
+    _in_progress_max_magnitude = max(_in_progress_max_magnitude, magnitude)
+    _sample_throttled()
+    _print_throttled()
 
 
-func _process_throttled() -> void:
+func _sample_throttled() -> void:
+    latest_magnitude = _in_progress_max_magnitude
+    _in_progress_max_magnitude = 0
+
+
+func _print_throttled() -> void:
     if log_mic_debugging:
-        S.log.print("Current mic amplitude (throttled): %s" % _latest_max_magnitude)
-    _latest_max_magnitude = 0
+        S.log.print("Current mic magnitude (throttled): %s" % _in_progress_max_magnitude)
+
+
+# [0,1]
+func get_blow_weight() -> float:
+    var magnitude: float = clamp(latest_magnitude, LOW_MAGNITUDE, HIGH_MAGNITUDE)
+    return (magnitude - LOW_MAGNITUDE) / (HIGH_MAGNITUDE - LOW_MAGNITUDE)
