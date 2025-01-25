33dd47470e7b475fe53f87ee495808b52af6ad0c
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-01-24 20:19:02 -0800

    Add some zoom calculations

--- src/characters/player/player.gd
+++ src/characters/player/player.gd
@@ -1,6 +1,22 @@
 class_name Player
-extends Node
+extends CharacterBody2D
+
+
+
+@export_range(0.0, 1.0) var initial_bubble_weight := 0.5
+
+@export var max_bubble_acceleration := 800.0
+
+@export var gravity_acceleration := 200.0
+
+# [0,1]
+var bubble_weight := 0.0
 
 
 func _init() -> void:
     S.player = self
+    bubble_weight = initial_bubble_weight
+
+
+func _physics_process(delta: float) -> void:
+    G.mic.get_blow_weight()
--- src/levels/main_level.gd
+++ src/levels/main_level.gd
@@ -2,7 +2,16 @@ class_name MainLevel
 extends Level
 
 
+const VIEWPORT_SIZE_BASIS := Vector2(576, 324)
+
 var horizontal_speed := 200
+
+
+func _ready() -> void:
+    var viewport_size: Vector2 = G.main.get_viewport().size
+    var viewport_basis_ratio := viewport_size / VIEWPORT_SIZE_BASIS
+    var zoom: float = max(viewport_basis_ratio.x, viewport_basis_ratio.y)
+    %Camera2D.zoom = Vector2.ONE * zoom
 
 
 func _physics_process(delta: float) -> void:
